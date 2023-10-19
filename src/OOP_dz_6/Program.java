b7159ddb9382bce8e3aff00ae23b1af400b4f078
Anna <132043882+anjaluchkina@users.noreply.github.com>
2023-10-19 20:18:01 +0300

    Update Program.java

--- src/OOP_dz_6/Program.java
+++ src/OOP_dz_6/Program.java
@@ -2,12 +2,6 @@ package OOP_dz_6;
 
 public class Program {
 
-    /**
-     * TODO: Переработать структуру приложения Order, приложение должно соответствовать
-     *  принципу SRP.
-     *
-     * @param args
-     */
     public static void main(String[] args) {
         OrderProcessing inputHandler = new OrderProcessing();
         Order order = new Order(inputHandler);
